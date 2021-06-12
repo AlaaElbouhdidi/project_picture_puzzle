97cf0337ce5a75f334a1801c8165a19e97fdde31
Mourad <mouradbhf@gmail.com>
2021-06-12 17:58:59 +0200

    module-picker hinzugefügt

---
+++ src/app/module/module-picker/module-picker.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-module-picker',
+  templateUrl: './module-picker.page.html',
+  styleUrls: ['./module-picker.page.scss'],
+})
+export class ModulePickerPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
