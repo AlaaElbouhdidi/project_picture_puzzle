c13c97970503580efec5d75089e59d3d89331485
Tim <81222680+timpez@users.noreply.github.com>
2021-06-29 08:37:29 +0200

    Achievements Styling angepasst + Model hinzugefügt

--- src/app/achievement/achievement-page/achievement-page.page.ts
+++ src/app/achievement/achievement-page/achievement-page.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ToastController } from '@ionic/angular';
+import {AlertController, ToastController} from '@ionic/angular';
+import {Achievement} from '../achievement.model';
 
 @Component({
   selector: 'app-achievement-page',
@@ -9,12 +10,15 @@ import { ToastController } from '@ionic/angular';
 export class AchievementPagePage implements OnInit {
 
   public playAchievement: boolean = false;
+  userAchievements: Achievement[];
 
-  constructor(private toastCtrl: ToastController) { }
+  constructor(private toastCtrl: ToastController, private alertController: AlertController) {
+    this.userAchievements = this.loadTestAchievements();
+  }
 
   ngOnInit() {
   }
-  
+
   displayAchievement(){
   this.toastCtrl.create({
     message: 'Achievement unlocked!',
@@ -30,4 +34,27 @@ export class AchievementPagePage implements OnInit {
   }, 2000);
 
   }
+
+  async showAchievementInfo(achievement: Achievement): Promise<void> {
+      const alert = await this.alertController.create({
+        cssClass: 'default-alert',
+        header: achievement.text,
+        subHeader: `Received at: ${achievement.date}`,
+        message: achievement.description,
+        buttons: ['OK']
+      });
+      await alert.present();
+  }
+
+  loadTestAchievements(): Achievement[] {
+    return [
+      new Achievement('1', 'Amateur', 'assets/awards/Amateur.png', 'This is a description placeholder', new Date().toLocaleDateString()),
+      new Achievement('2', 'Beginner', 'assets/awards/Beginner.png', 'This is a description placeholder', new Date().toLocaleDateString()),
+      new Achievement('3', 'Professional', 'assets/awards/Professional.png', 'This is a description placeholder', new Date().toLocaleDateString()),
+      new Achievement('4', 'World Class', 'assets/awards/WorldClass.png', 'This is a description placeholder', new Date().toLocaleDateString()),
+      new Achievement('5', 'Legendary', 'assets/awards/Legendary.png', 'This is a description placeholder', new Date().toLocaleDateString()),
+      new Achievement('6', 'Ultimate', 'assets/awards/Ultimate.png', 'This is a description placeholder', new Date().toLocaleDateString())
+    ]
+  }
+
 }
--- src/app/achievement/achievement.model.ts
+++ src/app/achievement/achievement.model.ts
@@ -1,3 +1,15 @@
 export class Achievement {
+  id: string;
+  text: string;
+  icon: string;
+  description: string;
+  date?: string;
 
+  constructor(id: string, text: string, icon: string, description: string, date?: string) {
+    this.id = id;
+    this.text = text;
+    this.icon = icon;
+    this.description = description;
+    this.date = date;
+  }
 }
